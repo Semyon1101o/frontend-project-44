3c1a7fe73d5c385c4db90c38d71ac9e894786bac
Semyon1101o <semen.fom.100@gmail.com>
2024-02-02 12:13:50 +0300

    gcd finally works

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { getQuestionAndAnswer } from './bin/brain-calc.js';
-
 const roundsCount = 3;
 
-const playGame = (rule) => {
+const playGame = (rule, getQuestionAndAnswer) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
@@ -13,15 +11,15 @@ const playGame = (rule) => {
   let correctAnswersCount = 0;
 
   while (correctAnswersCount < roundsCount) {
-    const [question, correctAnswer] = getQuestionAndAnswer();
-    console.log(`Question: ${question}`);
+    const correctAnswer = getQuestionAndAnswer();
+    console.log(`Question: ${correctAnswer[0]}`);
     const userAnswer = readlineSync.question('Your answer: ');
 
-    if (correctAnswer.toString() === userAnswer) {
+    if (correctAnswer[1].toString() === userAnswer) {
       console.log('Correct!');
       correctAnswersCount += 1;
     } else {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer[1]}".`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
