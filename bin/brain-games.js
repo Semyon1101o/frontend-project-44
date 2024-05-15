10089d18de995b80f86e7f04e32ad037fd27c4b5
Semyon1101o <semen.fom.100@gmail.com>
2024-05-15 11:15:39 +0300

    final version?

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-const playGame = (rule, getQuestionAndAnswer) => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+const playGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 playGame();
